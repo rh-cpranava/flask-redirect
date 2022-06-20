02e81c967a3cb248fb20e7d5036ffca8fd4424d1
GTPE Student <lab-user@bastion.zcbsn.internal>
2022-06-20 14:48:51 +0000

    Added code for testing

--- flask_redirect.py
+++ flask_redirect.py
@@ -4,50 +4,18 @@ flask_redirect: Redirects requests to appropriate URL
 """
 import os
 import requests
-from flask import Flask, redirect, render_template  # From module flask import class Flask
-app = Flask(__name__)    # Construct an instance of Flask class for our webapp
-
+import socket
+from datetime import datetime
+from flask import Flask, redirect, render_template  
+app = Flask(__name__)    
+port=5000
 @app.route('/')
 def base_function():
-    if "POD_NAME" in os.environ:
-        pod_name = os.environ.get('POD_NAME')
-    else:
-        return render_template('error.html')
-    
-    if "NODE_NAME" in os.environ:
-        node_name = os.environ.get('NODE_NAME')
-    else:
-        return render_template('error.html')
-    
-    if "POD_NAMESPACE" in os.environ:
-        namespace_name = os.environ.get('POD_NAMESPACE')
-    else:
-        return render_template('error.html')
-    
-    if "POD_IP" in os.environ:
-        pod_ip = os.environ.get('POD_IP')
-    else:
-        return render_template('error.html')
-    
-    if "POD_SERVICE_ACCOUNT" in os.environ:
-        pod_service_account = os.environ.get('POD_SERVICE_ACCOUNT')
-    else:
-        return render_template('error.html')
-    
-    return {
-        "pod_name": pod_name,
-        "node_name": node_name,
-        "namespace_name": namespace_name,
-        "pod_ip": pod_ip,
-        "pod_service_account": pod_service_account,
-    }
+    return {"ip_address": socket.gethostbyname(socket.gethostname()), "hostname": socket.gethostname(), "port": port, "date": datetime.now().strftime("%d%m%y") }
 
-
-@app.route('/status')
-def status():
-    base_url = os.environ.get('BASE_URL')
-    return_value = requests.get(base_url)
-    return return_value.json()
+@app.route('/status/<redirect_url>')
+def status(redirect_url):
+    return redirect('https://'+redirect_url.replace('-','.'))
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=True)
