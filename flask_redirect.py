aecd67b01122948ca32cf8075abb8d536dceccb5
Chandrahasa Pranava <81606179+rh-cpranava@users.noreply.github.com>
2023-04-27 21:27:39 +0800

    ipv6 support

--- flask_redirect.py
+++ flask_redirect.py
@@ -50,4 +50,4 @@ def status():
     return return_value.json()
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
+    app.run(host="::", port=5000, debug=True, use_reloader=True)
